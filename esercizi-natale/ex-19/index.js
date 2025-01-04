8831fc154268fd3a18f631b41e712ca08e14d8ee
Stefanospedicato <stefano.spedicato11@gmail.com>
2025-01-04 15:00:21 +0100

    feat: ex-19 complete

--- esercizi-natale/ex-19/index.js
+++ esercizi-natale/ex-19/index.js
@@ -4,3 +4,20 @@
 
   Puoi utilizzare sia fetch che axios per la chiamata AJAX.
 */
+
+const express = require('express')
+const app = express()
+const port = 3000
+const axios = require('axios')
+const endpoint = 'https://gerry-quotes.fly.dev/quotes/random'
+
+for(let i = 0 ; i < 5 ; i++){
+  
+  axios.get(endpoint)
+    .then(res=>{
+      console.log(res.data)
+    })
+    .catch(err=>{
+      console.log(err);
+    })
+}
