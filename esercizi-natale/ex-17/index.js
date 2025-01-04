b854642ee4d869b5ded665e65b6881e3300d3ce1
Stefanospedicato <stefano.spedicato11@gmail.com>
2025-01-04 14:30:54 +0100

    feat: ex-17 complete

--- esercizi-natale/ex-17/index.js
+++ esercizi-natale/ex-17/index.js
@@ -10,3 +10,14 @@
 */
 
 const data = "2024-12-18";
+
+function countdownNatale(data) {
+  const natale = new Date("2024-12-25")
+  const differenza = natale - data;
+  const unGiorno = 1000 * 60 * 60 * 24
+  const giorniMancanti = Math.ceil(differenza / unGiorno);
+  return giorniMancanti;
+}
+
+const date = new Date("2024-12-18");
+console.log(`Mancano ${countdownNatale(data)} giorni a Natale.`);
